6b017a0d3e1cc3bc1e3523f702ed4f6bde648859
Daniel Chaves <danielchavesfa@hotmail.com>
2025-08-11 20:16:14 -0300

    configuração com o mysql ef

--- Program.cs
+++ Program.cs
@@ -1,8 +1,20 @@
+using MinimalApi.Infraestrutura.Db;
+using MinimalApi.DTOs;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+  options.UseMySql(
+      builder.Configuration.GetConnectionString("mysql"),
+      ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 app.MapGet("/", () => "Hello World!");
-app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) =>
+app.MapPost("/login", (LoginDTO loginDTO) =>
 {
   if (loginDTO.Email == "admin@test.com" && loginDTO.Senha == "112233")
     return Results.Ok("Login com sucesso");
