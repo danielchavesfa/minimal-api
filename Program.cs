63f4b4be1f9b7a7ef44468b4ca8aaee995cc06a5
Daniel Chaves <danielchavesfa@hotmail.com>
2025-08-22 20:12:09 -0300

    feat: adiciona rota post para cadastrar novos veiculos

--- Program.cs
+++ Program.cs
@@ -5,12 +5,15 @@ using MinimalApi.Dominio.Interfaces;
 using MinimalApi.Dominio.Servicos;
 using Microsoft.AspNetCore.Mvc;
 using MinimalApi.Dominio.ModelViews;
+using MinimalApi.Dominio.Entidades;
 
+#region Builder
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
+builder.Services.AddScoped<IVeiculoServico, VeiculoServico>();
 builder.Services.AddDbContext<DbContexto>(options =>
 {
   options.UseMySql(
@@ -20,13 +23,18 @@ builder.Services.AddDbContext<DbContexto>(options =>
 });
 
 var app = builder.Build();
+#endregion
 
+#region Home
 app.MapGet("/", () =>
 {
   return Results.Json(new Home());
 });
+#endregion
 
-app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
+#region Administradores
+
+app.MapPost("/adm/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
 {
   if (administradorServico.Login(loginDTO) != null)
     return Results.Ok("Login com sucesso");
@@ -34,8 +42,29 @@ app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico admin
     return Results.Unauthorized();
 });
 
+#endregion
+
+#region Veículos
+
+app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) =>
+{
+  var novoVeiculo = new Veiculo
+  {
+    Nome = veiculoDTO.Nome,
+    Marca = veiculoDTO.Marca,
+    Ano = veiculoDTO.Ano
+  };
+
+  veiculoServico.Incluir(novoVeiculo);
+
+  return Results.Created($"/veiculo/{novoVeiculo.Id}", novoVeiculo);
+});
+
+#endregion
+
+#region App
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.Run();
-
+#endregion
